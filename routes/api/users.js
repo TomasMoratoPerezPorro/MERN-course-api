2299eb52be1356759588cab04fb28eae56881fca
TomasMoratoPerezPorro <tomasmpp@gmail.com>
2020-11-23 18:21:50 +0100

    User Registration endpoint

--- routes/api/users.js
+++ routes/api/users.js
@@ -1,10 +1,76 @@
 const express = require("express");
 const router = express.Router();
+const gravatar = require("gravatar");
+const bcrypt = require("bcryptjs");
+const { check, validationResult } = require("express-validator");
 
-// @route           GET api/users
-// @description     Test Route
+const User = require("../../models/User");
+
+// @route           POST api/users
+// @description     Register a user
 // @access          Public
 
-router.get("/", (req, res) => res.send("User route"));
+router.post(
+  "/",
+  [
+    check("name", "Name is required").not().isEmpty(),
+    check("email", "Please enter a valid email").isEmail(),
+    check("password", "Enter a password with more than 6 characters").isLength({
+      min: 6,
+    }),
+  ],
+  async (req, res) => {
+    const errors = validationResult(req);
+    if (!errors.isEmpty()) {
+      return res.status(400).json({ errors: errors.array() });
+    }
+
+    const { name, email, password } = req.body;
+
+    try {
+      // 1 Check if the user exists
+      let user = await User.findOne({ email });
+
+      if (user) {
+        return res
+          .status(400)
+          .json({ errors: [{ msg: "User already exists" }] });
+      }
+
+      // 2 Get users gravatar
+
+      const avatar = gravatar.url(email, {
+        s: "200",
+        r: "pg",
+        d: "mm",
+      });
+
+      user = new User({
+        name,
+        email,
+        avatar,
+        password,
+      });
+
+      // 3 Encrypt password
+
+      const salt = await bcrypt.genSalt(10);
+
+      user.password = await bcrypt.hash(password, salt);
+
+      await user.save();
+
+      // 4 Return jsonwebtoken
+
+      res.send("User registered");
+    } catch (err) {
+      console.error(err.message);
+      res.status(500).send("Server error");
+    }
+
+    console.log(req.body);
+    res.send("User route");
+  }
+);
 
 module.exports = router;
--- server.js
+++ server.js
@@ -6,6 +6,9 @@ const app = express();
 //Connect database
 connectDB();
 
+//Init Middleware
+app.use(express.json({ extended: false }));
+
 //Test Endpoint
 app.get("/", (req, res) => res.send("API Running"));
 
