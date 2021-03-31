a7aa4320241af02502d78fc905009cd9ebcb2243
Tomas Morato <tomas.morato@between.tech>
2021-03-31 10:25:31 +0200

    User Login and redirection to dashboard on both login and register components

--- client/src/components/auth/Login.js
+++ client/src/components/auth/Login.js
@@ -1,9 +1,13 @@
-import React, { Fragment, useState } from "react";
-import {Link} from 'react-router-dom';
-const Register = () => {
+import React, { Fragment, useState } from 'react';
+import { Link, Redirect } from 'react-router-dom';
+import { connect } from 'react-redux';
+import PropTypes from 'prop-types';
+import { login } from '../../actions/auth';
+
+const Login = ({ login, isAuthenticated }) => {
   const [formData, setFormData] = useState({
-    email: "",
-    password: "",
+    email: '',
+    password: '',
   });
 
   const { email, password } = formData;
@@ -13,50 +17,62 @@ const Register = () => {
 
   const onSubmit = async (e) => {
     e.preventDefault();
-    console.log("Success");
+    login(email, password);
   };
+
+  if (isAuthenticated) {
+    return <Redirect to='/dashboard' />;
+  }
 
   return (
     <Fragment>
-      <h1 className="large text-primary">Sign In</h1>
-      <p className="lead">
-        <i className="fas fa-user"></i> Login to Your Account
+      <h1 className='large text-primary'>Sign In</h1>
+      <p className='lead'>
+        <i className='fas fa-user'></i> Login to Your Account
       </p>
-      <form className="form" onSubmit={(e) => onSubmit(e)}>
-        
-        <div className="form-group">
+      <form className='form' onSubmit={(e) => onSubmit(e)}>
+        <div className='form-group'>
           <input
-            type="email"
-            placeholder="Email Address"
-            name="email"
+            type='email'
+            placeholder='Email Address'
+            name='email'
             value={email}
             onChange={(e) => onChange(e)}
             required
           />
-          <small className="form-text">
+          <small className='form-text'>
             This site uses Gravatar so if you want a profile image, use a
             Gravatar email
           </small>
         </div>
-        <div className="form-group">
+        <div className='form-group'>
           <input
-            type="password"
-            placeholder="Password"
-            name="password"
+            type='password'
+            placeholder='Password'
+            name='password'
             value={password}
             onChange={(e) => onChange(e)}
             required
-            minLength="6"
+            minLength='6'
           />
         </div>
-        
-        <input type="submit" className="btn btn-primary" value="Legister" />
+
+        <input type='submit' className='btn btn-primary' value='Legister' />
       </form>
-      <p className="my-1">
-        You don't have an account ? <Link to="/register">Register</Link>
+      <p className='my-1'>
+        You don't have an account ? <Link to='/register'>Register</Link>
       </p>
     </Fragment>
   );
 };
 
-export default Register;
+Login.propTypes = {
+  login: PropTypes.func.isRequired,
+  isAuthenticated: PropTypes.bool,
+};
+
+const mapStateToProps = (state) => ({
+  isAuthenticated: state.auth.isAuthenticated,
+});
+
+export default connect(mapStateToProps, { login })(Login);
